0a0b91efcc4f05c299c75e1765400679ccc2a26a
amaliekea <amje0001@stud.kea.dk>
2024-04-03 10:43:02 +0200

    setup med test

---
+++ movieCollectionTest/domain_model/AddMovieTest.java
@@ -0,0 +1,41 @@
+package domain_model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddMovieTest {
+
+    @Test
+    void addMovie() {
+        //Arrange - setup our test objects.
+        MovieCollection movieCollection = new MovieCollection();
+        movieCollection.addMovie("Rose", "James", 2002, true, 120, "future");
+
+        //Act - Execute the method under test with the arranged conditions.
+        int expectedMovie = 1;
+        int actualMovie = movieCollection.filmObjekter.size();
+
+
+        //Assert -verify the actions's outcome is as expected
+        Assertions.assertEquals(expectedMovie, actualMovie);
+    }
+
+    @Test
+    void getMovieCollectionList() {
+        //Arrange - setup our test objects.
+        MovieCollection movieCollection = new MovieCollection();
+        movieCollection.addMovie("Silver", "James", 2002, true, 120, "future");
+
+        //Act - Execute the method under test with the arranged conditions.
+        ArrayList<Movie> expectedMovies = new ArrayList<Movie>();
+        expectedMovies.add(new Movie("Silver", "James", 2002, true, 120, "future"));
+        ArrayList<Movie> actualMovies = movieCollection.getMovieCollectionList();
+
+        //Assert -verify the actions's outcome is as expected
+        Assertions.assertArrayEquals(expectedMovies.toArray(), actualMovies.toArray());
+    }
+}
